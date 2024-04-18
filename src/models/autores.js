15f3c4944e4c6d50f8ee7121127ed113bbfac2c6
Lucas <lucas.donizet@gmail.com>
2024-04-17 21:31:28 -0300

    Criado dbConnect autores e livros .js

---
+++ src/models/autores.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const autorSchema = new mongoose.Schema({
+    id: {type: mongoose.Schema.Types.ObjectId},
+    nome:{type:String, required:true},
+    nacionalidade:{type:String},
+},{
+    versionKey:false //para que no banco de dados não apareça a versão do documento
+});
+
+const autor = mongoose.model("autores", autorSchema);
+
+export{autor, autorSchema};
